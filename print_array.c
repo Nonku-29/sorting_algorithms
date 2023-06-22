dd6e13a8705b7d33761e4a8ef15e54626003a9a2
Nonku-29 <122856617+Nonku-29@users.noreply.github.com>
2023-06-22 12:03:35 +0200

    Update print_array.c

--- print_array.c
+++ print_array.c
@@ -11,13 +11,12 @@ void print_array(const int *array, size_t size)
 {
 	size_t i;
 
-	i = 0;
-	while (array && i < size)
+	printf("Array elements: ");
+	for (i = 0; i < size; i++)
 	{
 		if (i > 0)
 			printf(", ");
 		printf("%d", array[i]);
-		++i;
 	}
 	printf("\n");
 }
